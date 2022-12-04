0a8786aa8c56445e0cc67392dc4adc283476d63a
noice <saranshh2@gmail.com>
2022-12-04 08:34:11 +0000

    Update 3 files

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-console.log("Saransh is coding");
+
+var cowsay = require("cowsay");
+const Quote = require('inspirational-quotes');
+
+console.log(cowsay.say({
+    text : Quote.getRandomQuote(),
+    e : "oO",
+    T : "U "
+}));
